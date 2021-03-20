1d06a24e3a8117c23a432274801826ac5d53cd26
MERVE BÜŞRA <mervebusradurmus19@gmail.com>
2021-03-20 16:14:42 +0300

    startangular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,48 @@ import { Component } from '@angular/core'; //c# kısmında yaptığımız using 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']  //array şeklinde olmasının nedeni birden fazla css dosyası olmasıdır.
+  styleUrls: ['./app.component.css'], //array şeklinde olmasının nedeni birden fazla css dosyası olmasıdır.
 })
 export class AppComponent {
-  title:string = 'northwind';  //javascriptte veri tipi vermemize gerek yok. Karşısında ne veriyorsak veri tipi odur.
+  title: string = 'northwind'; //javascriptte veri tipi vermemize gerek yok. Karşısında ne veriyorsak veri tipi odur.
   //typescriptte yukarıdaki gibi veri tipini verip tip güvenli hale getirebiliriz.çift veya tek tırnak kullanmamız farketmez.
-  
+  user: string = 'Engin Demiroğ';
+  product1: any = {
+    productId: 1,
+    productName: 'Bardak',
+    categoryId: 1,
+    unitPrice: 5,
+  }; //any varsa her veri tipi olabilir demektir.
+  product2: any = {
+    productId: 2,
+    productName: 'Laptop',
+    categoryId: 1,
+    unitPrice: 5,
+  };
+  product3: any = {
+    productId: 3,
+    productName: 'Mouse',
+    categoryId: 1,
+    unitPrice: 5,
+  };
+  product4: any = {
+    productId: 4,
+    productName: 'Keyboard',
+    categoryId: 1,
+    unitPrice: 5,
+  };
+  product5: any = {
+    productId: 5,
+    productName: 'Camera',
+    categoryId: 1,
+    unitPrice: 5,
+  };
+
+  products = [
+    this.product1,
+    this.product2,
+    this.product3,
+    this.product4,
+    this.product5,
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { CategoryComponent } from './components/category/category.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+  //bir module'ün bir componenti kullanabilmesi için o modulde declerasyonlarda componentlerin tanımlı olması lazım.
+  //bunlarıda burada kullanabilmek için yukarıda import etmek gerekli.
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
